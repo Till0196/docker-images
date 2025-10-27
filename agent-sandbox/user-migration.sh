ca4cc8bfa72713e66b29164796cfefa91dc22298
Till0196 <16399842+Till0196@users.noreply.github.com>
2025-10-27 18:48:40 +0900

    Dockerfile: replace entrypoint script with user-migration script

--- agent-sandbox/user-migration.sh
+++ agent-sandbox/user-migration.sh
@@ -13,13 +13,21 @@ if [ "$TARGET_USERNAME" != "$BUILD_USERNAME" ]; then
     usermod -l "$TARGET_USERNAME" -d "/home/$TARGET_USERNAME" "$BUILD_USERNAME"
 
     if [ -d "/home/$BUILD_USERNAME" ]; then
-        rsync -a "/home/$BUILD_USERNAME/" "/home/$TARGET_USERNAME/"
-        rm -rf "/home/$BUILD_USERNAME"
+        mv "/home/$BUILD_USERNAME" "/home/$TARGET_USERNAME"
     fi
+
+    if [ -f "/home/$TARGET_USERNAME/.zshrc" ]; then
+        sed -i "s|/home/$BUILD_USERNAME/|/home/$TARGET_USERNAME/|g" "/home/$TARGET_USERNAME/.zshrc"
+        sed -i "s|$BUILD_USERNAME|$TARGET_USERNAME|g" "/home/$TARGET_USERNAME/.zshrc"
+    fi
+
     chown -R "$TARGET_USERNAME":"$TARGET_USERNAME" \
         "/home/$TARGET_USERNAME" \
         /usr/local/share/npm-global \
         /commandhistory
+
+    echo "${TARGET_USERNAME} ALL=(ALL) NOPASSWD:ALL" > /etc/sudoers.d/${TARGET_USERNAME}-nopasswd && \
+    chmod 0440 /etc/sudoers.d/${TARGET_USERNAME}-nopasswd
 
     echo "Migration completed: $BUILD_USERNAME (uid:1000) -> $TARGET_USERNAME (uid:1000)"
 else
